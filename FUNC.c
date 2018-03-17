57ee849e8ba328b3d43ba722d17823224d603eca
MauryJohnson <37424385+MauryJohnson@users.noreply.github.com>
2018-03-17 00:20:09 -0400

    Update FUNC.c

--- FUNC.c
+++ FUNC.c
@@ -1,13 +1,16 @@
 #include "HEADER.h"
 
 
-
+//Read the directory given the path
 void ReadDir(char* Path,char* Path2, bool BASE){
 DIR* currentDir = NULL;
 struct dirent* currentThing=NULL;
-//if(access(Path2,R_OK)
+//Structures specifically designed to open directories
+
+//If not within base directory, or the preceeding path's name exists.
 if(strlen(Path2)>0||BASE==false){
 currentDir= opendir(Path2);
+//Check if path can be accessed, if not, free all used parameters and return.
 if(access(Path2,R_OK)!=0){
 
 if(Path!=NULL){
@@ -18,14 +21,19 @@ if(Path2!=NULL){
 free(Path2);
 Path2=NULL;
 }
-
 closedir(currentDir);
 return;
 }
+  
+//Set directory pointer to NULL
+
 currentThing=NULL;
 }
+  
 else{
+//Case where within base directory.
 currentDir= opendir(Path);
+//Check if path can be accessed, if not, free all used parameters and return.
 if(access(Path,R_OK)!=0){
 
 if(Path!=NULL){
@@ -40,49 +48,49 @@ Path2=NULL;
 closedir(currentDir);
 return;
 }
+//Set directory pointer to NULL.
 currentThing=NULL;
 }
 
+//char*'s used for string concatenations of pathname key characters like "." and "/".
 char* i=NULL;
 char* i2=NULL;
 
 do{
 if(currentDir!=NULL)
 currentThing=readdir(currentDir);
+//If readdir(currentDir) returns NULL, no more directories to read or an error occurred reading a directory.
 if(currentThing==NULL){
 printf("\nEnd of directory reading\n");
 break;
 }
+  
+//d_type's used will be DT_REG, which is a file, and DT_DIR, which is a directory.
+
 if(currentThing->d_type==DT_REG){
 //printf("\nFOUND A FILE\n");
 
 printf("\n %s \n",currentThing->d_name);
-//Only have to append /!!
-//CatFile
+  
+//If within the base directory.
 if(BASE==true){
 //Free Path Name
-
 if(strlen(Path)>0&&Path!=NULL&&sizeof(Path)>0){
 free(Path);
 Path=NULL;
 }
-
-//char** D = &(currentThing->d_name);
-//CatDir("./",D);
+//Set static d_name to a freeable string.  
 Path = toString(currentThing->d_name);
-
+  
 char* u = malloc(3*sizeof(char));
 u[0]='.';
 u[1]='/';
 u[2]='\0';
-//CatDir(u,&Path);
+
 strcat(u,Path);
 free(Path);
 Path=NULL;
 Path=u;
-
-
-//printf("PATH2 ACCESS:%s",Path2);
 
 if(access(Path,R_OK)==0){
 
@@ -90,28 +98,26 @@ printf("\nReadable File: %s\n",u);
 
 i = toString(Path);
 i2 = toString(Path);
-
+//"i" and "i2" used to insert char*'s into a linkes list which would later be freed, also Path is freed, so this is used to
+//not free data twice.
+//Takes copies of i and i2 char*'s, opens the path to filename, and inserts all tokens in a file into a linked list.
 Insert(i, i2, NULL);
-i=NULL;
 
+//Free the copied variables of Path.  
+i=NULL;
 free(i2);
 i2=NULL;
-
-//PrintList(".");
-
-//DeleteList();
-
-//exit(0);
 }
 else{
 
+//An unreadable file has been found.
 printf("\nUnreadable file\n");
-
+  
 }
 
 }
 else{
-//Dont free Path Name
+//Dont free Path Name.
 char* c = toString(currentThing->d_name);
 
 printf("PATH2 ACCESS:%s",Path2);
@@ -120,89 +126,73 @@ if(Path2[strlen(Path2)-1]!='/'){
 u=malloc(2*sizeof(char));
 u[0]='/';
 u[1]='\0';
+//Create path outside of the base to the current file
 CatFile(u,&c);
 }
+
+//Extend the path further
 strcat(Path2,c);
-///////////////////
-/*
-if(c!=NULL){
-free(c);
-c=NULL;
-}
-*/
+  
 int k=countOcc("/",&Path2)-1;
 
+
+//If the file does not exist in the next directory, go to the previous directory and try again, until reach the base directory.
 while(access(Path2,R_OK)!=0&&k>1){
 printf("\nUnreadable file...\n");
 PrevPath(&Path2,k);
 k=countOcc("/",&Path2)-1;
 strcat(Path2,c);
 }
+//Check if file is readable  
 if(access(Path2,R_OK)==0){
 printf("\nReadable File... Using %s\n",Path2);
 
+//Create copies of the file path name
 i = toString(Path2);
 i2 = toString(Path2);
-
+//Insert the file path name copies into linked list
 Insert(i, i2, NULL);
 
-//free(i);
 i=NULL;
 free(i2);
 i2=NULL;
 }
 else{
-printf("\nUNABLE TO FIND FILE,ERROR\n");
-break;
+//File could not be found given the path.
+printf("\nFile cannot be read\n");
 }
 printf("\n FILE THAT IS NOT IN BASE: %s\n", Path2);
 printf("NUMBER OF /:%d",countOcc("/",&Path2));
 free(c);
 c=NULL;
-//return;
+//If the file does not exist in the next directory, go to the previous directory and try again, until reach the base directory.
 PrevPath(&Path2,countOcc("/",&Path2));
-//return;
 printf("DECREASED PATH2:%s",Path2);
-//return;
-//exit(0);
 }
 }
 
 
 else if(currentThing->d_type==DT_DIR){
 printf("\n%s\n",currentThing->d_name);
-//Wher Hit next Dir outside of BAS)E
+//When hit next Dir outside of BASE
 if(strcmp(currentThing->d_name,".")==0||strcmp(currentThing->d_name,"..")==0){
 continue;
 }
 
-//str
 if(BASE==true){
-// Append 
-//if(strlen(Path2)>1){
-//Delete Path
-
-/*
-free(Path2);
-Path2=NULL;
-//char* q =NULL;
-Path2= malloc(sizeof(char));
-Path2[0]='\0';
-*/
-//Path2[0]='\0';
-//Path2=toString("\0");
-//}
-
+//Create a string of the directory name
 char* s = toString(currentThing->d_name);
 
+//Concatenate the next path to the directory name
 strcat(Path2,s);
 
+//Free copy of s
 free(s);
 s=NULL;
 
-//Path2=q;
 printf("PATH2: %s",Path2);
 
+//If the last character of the path does not contain a '/', concatenate a "./" to the path. 
 if(Path2[strlen(Path2)-1]!='/'){
 
 char* u = malloc(3*sizeof(char));
@@ -211,26 +201,20 @@ u[0]='.';
 u[1]='/';
 u[2]='\0';
 
-//
-//CatDir(u,&Path2);
 strcat(u,Path2);
 free(Path2);
 Path2=NULL;
 Path2=u;
 }
-
+//Recursively iterate to the next directory, while still iteratig through all other directories
+//and files not visited within current directory, false flag for BASE, because just opened another directory.
 ReadDir(toString(Path),toString(Path2),false);
-//free(Path2);
-//Path2=NULL;
 }
 
-
 else{
-
-// Append / to next Dir!
-//CatFile("/",&Path2);
+//Copy static directory name to a character pointer.
 char* c = toString(currentThing->d_name);
-
+//If the path does not contain a '/' at the end, now concatenate a / to the path.
 if(Path2[strlen(Path2)-1]!='/'){
 char* u = malloc(2*sizeof(char));
 u[0]='/';
@@ -243,25 +227,21 @@ free(c);
 c=NULL;
 }
 else{
-break;}
+printf("\nNo Path to go to next.\n");
+break;
+}
 printf("\n\nPATH 2: %s\n\n",Path2);
-//exit(0);
-//ReadDir(Path,Path2,false);
-//exit(0);
-//closedir(currentDir);
 
-//STRING VERY IMPORTANT
-//ReadDir((Path),(Path2),false);
+//Recursive iteration to next directory.
 ReadDir(toString(Path),toString(Path2),false);
-//TO STRING FTW
+}
 }
 
-}
-
-//printf("PATH 2:%s",Path2);
 printf("\n\n");
 }while(currentThing!=NULL);
+//While directory pointer is not NULL.
 
+//Free variables used for copying strings if they are not freed already.
 if(i2!=NULL){
 free(i2);
 i2=NULL;
@@ -270,7 +250,7 @@ if(i!=NULL){
 free(i);
 i=NULL;
 }
-
+//Free variables used for copying strings if they are not freed already.
 if(Path!=NULL){
 free(Path);
 Path=NULL;
@@ -280,19 +260,14 @@ free(Path2);
 Path2=NULL;
 }
 
+//Close the directory iterated upon
 closedir(currentDir);
-
-//exit(0);
+//
 return;
 }
-//
-//void CatDir(char* app,char** C);
-//
-//void CatFile(char* app,char**C);
-//
-//
+//Concatinates a string to the second argument string provided.
 void CatDir(char* app, char** C){
-
+  
 if(C==NULL){
 return;
 }
@@ -300,36 +275,15 @@ if(*C==NULL){
 
 return;
 }
-
-//int i = strlen(*C);
-//char* z = malloc(strlen(app)*sizeof(char));
-
-//strcpy(z,app);
+//Concatenate the string to the second argument
 strcat(app,*C);
-/*
-if(strlen(*C)>0){
 free(*C);
-(*C)=NULL;
-}
-*/
-free(*C);
-/*
-(*C)=NULL;
-(*C)=app;
-*/
 return;
 }
 
-
+//Concatenate a string to the second argument string provided.
 void CatFile(char* app, char** C){
-//char*z=malloc(strlen(app)*sizeof(char));
-//z[strlen(app)-1]='\0';
-//strcpy(z,app);
-//z[strlen(app)-1]='\0';
-//free(app);
-//app=NULL;
-
-//strcat(z,(*C));
+  
 strcat(app,(*C));
 
 free(*C);
@@ -353,10 +307,9 @@ u=C[i];
 }
 
 return v;
-//free(v);
 }
 
-
+//Returns true if the file path is in the directory, false if not.
 bool FileinDir(char* Path){
 struct dirent* currentThing;
 DIR* Directory = opendir(".");
@@ -374,24 +327,20 @@ return true;
 }
 }while(currentThing!=NULL);
 
-
-
 return false;
 }
 
-//Go to the NextLth line!!
+//Go to the previous directory string given the count of directories by int NextL.
 void PrevPath(char**C,int NextL){
 int occ=0;
 int i=strlen(*C);
 printf("LENGTH: %d",i);
-//char c = (*C)[i];
 int j=0;
+  
 char* e = malloc(2*sizeof(char));
 e[0] =(*C)[j];
 e[1]='\0';
-//int countL = 0;
-
-
+  
 char* g=malloc(sizeof(char));
 g[0]='\0';
 
@@ -418,12 +367,11 @@ free(*C);
 free(e);
 e=NULL;
 
-
 return;
 }
 
+//Counts the occurrences of a character or string, argument 1, within a string, argument 2.
 int countOcc(char* n, char**C){
-//int i = strlen(n);
 int j=0;
 int k=0;
 int count=0;
@@ -444,17 +392,12 @@ c = (*C)[j];
 }
 
 return count;
-
 }
 
 //Will READ EVERY FILE
 void Insert(char* Dir, char* FileName, char** Names){
 //Dir and filename go hand in hand
 Occurrences** D =&O;
-
-//Occurrences ** OCC = D;
-
-//Occurrences ** OCC2=D;
 
 while(*D!=NULL){
 if(strcmp((*D)->FileName,FileName)==0){
@@ -471,15 +414,18 @@ if(strcmp((*D)->FileName,FileName)==0){
 //ADD THIS FileName onto same filename list, just increase (Names)
 //BE SURE TO USE PATH NAME TO OPEN FILE
 FILE* F = fopen(FileName,"r");
+//An error occurred while opening the file.
 if(F==NULL){
 printf("ERROR, CANNOT READ FILE");
 return;
 }
 
+//variable that is used to append pieces of tokens into a token word.
 char* app = malloc(2*sizeof(char));
 app[0]=' ';
 app[1]='\0';
 
+//Word which will be a complete token to be inserted into a linked list.
 char* Word = malloc(sizeof(char));
 Word[0]='\0';
 
@@ -487,6 +433,7 @@ int wordSize = 1;
 
 bool isReadable = true;
 
+//Check if the file contains all readable characters.  
 while(fscanf(F, "%c", &(app[0])) != EOF){
 if((int)app[0]<0||(int)app[0]>126){
 printf("\nError, file is not readable\n");
@@ -499,19 +446,20 @@ break;
 
 fclose(F);
 
+//If the file is readable, proceed to read the file.
 if(isReadable==true){
 
 printf("\n File: %s is readable, reading...\n",FileName);
 
 F=fopen(FileName,"r");
+//If the file cannot be opened
 if(F==NULL){
 printf("Error, cannot read file");
 return;
 }
-
+//Use fscanf to read through file character by character and read all tokens which are characters in the alphabet, or numbers.
+//Delimiters will be any character that does not fall under the category of a token.
 while(fscanf(F, "%c", &(app[0])) != EOF){
-
-
 
 printf("%c",app[0]);
 
@@ -522,15 +470,19 @@ Word=realloc(Word,2*sizeof(char));
 Word[wordSize-1]='\0';
 strcat(Word,app);
 wordSize++;
-//TOKENZ
+//TOKENS
 }
 
 else if(Word[0]!='\0'){
 //FOUND A WORD
 
 char c = '/';
+
+//Compress string to be just the filename, IncrOcc checks if the filename already was found and just adds and increments
+//The word could of the word. If the word is also the same, it increments the word count of that word.
+//IncrOcc returns 0 when there is a new word found, and not 0 when the same word is found.
 if(IncrOcc(&O,Word,strrchr(strrchr(FileName,c),(strrchr(FileName,c)[1])))==0){
-//Found none, NEW WORD
+//NEW WORD
 while(*D!=NULL){
 D=&((*D)->Next);
 }
@@ -552,15 +504,13 @@ PrintList("");
 
 }
 
-
+//Clear Word and set it to size 1 again.
 free(Word);
 Word=NULL;
 Word = malloc(sizeof(char));
 Word[0]='\0';
 wordSize=1;
-//IncrOcc(OCC,Word);
-//FIND OUT IF ALREADY EXISTS
-//Not a char or a num... Potentially a space!
+//If not a char or a num, it's a delimiter!
 }
 
 }
@@ -570,6 +520,7 @@ printf("\n File: %s is not readable\n",FileName);
 }
 
 printf("\n\nEND FILE STREAM\n\n");
+//Free the word and appender data
 free(Word);
 Word=NULL;
 free(app);
@@ -581,10 +532,11 @@ fclose(F);
 }
 
 else if(*D==NULL){
-
+//Found a new file to read.
 printf("\nCREATE NEW LIST\n");
-//Create a new
+//Create a new list.
 
+//If the file can be accessed.
 if(access(FileName,R_OK)==0){
 
 printf("\nSEARCH FILENAME: %s IN DIRECTORY: %s\n",FileName,Dir);
@@ -602,7 +554,7 @@ app[1]='\0';
 
 
 bool isReadable=true;
-
+//Check if file contains all readable characters, if not, set isReadable flag to false.
 while(fscanf(F, "%c", &(app[0])) != EOF){
 if((int)app[0]<0||(int)app[0]>126){
 printf("\nFile is not readable\n\n");
@@ -616,7 +568,7 @@ fclose(F);
 char* Word = malloc(sizeof(char));
 Word[0]='\0';
 
-
+//Only read through file if the file is completely readable.
 if(isReadable==true){
 
 printf("\n File: %s is readable, reading...\n",FileName);
@@ -630,6 +582,8 @@ return;
 
 
 int wordSize = 1;
+//Use fscanf to read through file character by character and read all tokens which are characters in the alphabet, or numbers.
+//Delimiters will be any character that does not fall under the category of a token.
 while(fscanf(F, "%c", &(app[0])) != EOF){
 
 
@@ -648,6 +602,9 @@ wordSize++;
 else if(Word[0]!='\0'){
 //FOUND A WORD
 char c = '/';
+//Compress string to be just the filename, IncrOcc checks if the filename already was found and just adds and increments
+//The word could of the word. If the word is also the same, it increments the word count of that word.
+//IncrOcc returns 0 when there is a new word found, and not 0 when the same word is found.
 if(IncrOcc(&O,Word,strrchr(strrchr(FileName,c),(strrchr(FileName,c)[1])))==0){
 //Found none, NEW WORD
 (*D)=malloc(sizeof(Occurrences));
@@ -689,6 +646,7 @@ printf("\n File: %s is not readable\n",FileName);
 }
 printf("\n\nEND FILE STREAM\n\n");
 
+//Free char* arrays used to store copies of word tokens of a particular file into linked list.
 free(Word);
 Word=NULL;
 
@@ -696,8 +654,6 @@ free(app);
 app=NULL;
 //Finished with file
 fclose(F);
-
-//exit(0);
 }
 else{
 printf("\nHAVE NO ACCESS TO FILE: %s\n",FileName);
@@ -713,11 +669,11 @@ return;
 }
 
 void PrintList(char* arg1){
-//FILE*F = fopen
-
+  
 printf("\nPRINT LIST\n\n");
 
-Occurrences** OCC =&O; //&((*G)->Occurrence);
+Occurrences** OCC =&O;
+  
 while(*OCC!=NULL){
 if((*OCC)!=NULL){
 if((*OCC)->Name!=NULL){
@@ -767,6 +723,7 @@ return;
 int IncrOcc(Occurrences** OCC, char* Word,char* FileName){
 int count=0;
 //If same word is found, increment the occurrence!
+
 while(*OCC!=NULL){
 if(strcmp((*OCC)->Name,Word)==0&&strcmp((*OCC)->FileName,FileName)==0){
 //FOUND
@@ -782,10 +739,9 @@ return count;
 }
 
 void PutFile(FILE*F){
-//JUST A FILE
+//JUST PUT STRINGS INTO A FILE.
 
 char** Visited = malloc(sizeof(char**));
-//Visited[0]=malloc(sizeof(char*));
 int i=0;
 
 bool V = false;
@@ -793,8 +749,10 @@ bool V = false;
 Occurrences** U = &O;
 PUT(F,1,NULL,0);
 PUT(F,2,NULL,0);
+//Put header information into desired file.
 char* ALL;
 Occurrences** Back = &O;
+
 while(*U!=NULL){
 ALL = (*U)->Name;
 Back=&((*U)->Next);
@@ -807,7 +765,8 @@ else{
 V=true;
 }
 }
-
+//If the file was not already visited, under the strings desired outputted files, place the information of the file and the token(s)
+//Found along with how many times they occurred.
 if(V==false){
 PUT(F,4,NULL,0);
 PUT(F,6,(*U)->Name,(*U)->Occurrence);
@@ -815,7 +774,8 @@ PUT(F,6,(*U)->Name,(*U)->Occurrence);
 
 
 while((*U)!=NULL){
-
+//If the file was not already visited, under the strings desired outputted files, place the information of the file and the token(s)
+//Found along with how many times they occurred.
 if(strcmp((*U)->Name,ALL)==0&&isVisited(Visited,(*U)->Name,i)==false){
 PUT(F,7,(*U)->Name,(*U)->Occurrence);
 
@@ -826,26 +786,30 @@ PUT(F,8,(*U)->FileName,(*U)->Occurrence);
 PUT(F,9,(*U)->Name,(*U)->Occurrence);
 
 }
+//Next Occurrences* address.
 U=&((*U)->Next);
 }
+//If the file was not visited, write </file>
 if(V==false)
 PUT(F,5,NULL,0);
 
+//Add more data visited list and zero out the new space.
 Visited[i]=malloc((strlen(ALL)+1)*sizeof(char));
 bzero(Visited[i],(strlen(ALL)+1));
-//Visited[i]=ALL;
+//Copy the string of the next file to the list of visited files.
 strcpy(Visited[i],ALL);
 printf("\n\nVISITED COPIED: %s\n\n",Visited[i]);
+//Increment i to prepare for the next visited entry.
 i++;
-Visited[i]=malloc(sizeof(char*));
+//Visited[i]=malloc(sizeof(char*));
 U=Back;
 }
+//Write to file </fileIndex>
 PUT(F,3,NULL,0);
-//free(Visited);
-//freeVisited
 return;
 }
 
+//Puts certain strings into a file specified by the type, the name of file or token used, and the count of the tokens
 void PUT(FILE*F, int type, char* Name, int count){
 char* g = "<?xml version= \"1.0\" encoding=\"UTF-8\"?>";
 char* f = "<fileIndex>";
@@ -877,8 +841,6 @@ fputc(c,F);
 i++;
 c=f[i];
 }
-//fputc('\n',F);
-//fputc('\t',F);
 }else if(type==3){
 c=h[0];
 i=0;
@@ -904,12 +866,7 @@ fputc(c,F);
 i++;
 c=k[i];
 }
-//fputc('\n',F);
-
-//fputc('\t',F);
-
-//fputc('\t',F);
-
+  
 }else if(type==5){
 c=j[0];
 i=0;
@@ -921,16 +878,10 @@ fputc(c,F);
 i++;
 c=j[i];
 }
-//fputc('\n',F);
-
-//fputc('\t',F);
-
 }
 else if(type==6){
 c=Name[0];
 i=0;
-//fputc('\"',F);
-
 
 while(c!='\0'){
 fputc(c,F);
@@ -940,18 +891,12 @@ c=Name[i];
 
 fputc('\"',F);
 fputc('>',F);
-
-//fputc('\n',F);
-
-//fputc('\t',F);
-
+  
 }
 else if(type==7){
 c=l[0];
 i=0;
-
-//fputc('\t',F);
-
+  
 fputc('\n',F);
 fputc('\t',F);
 fputc('\t',F);
@@ -961,28 +906,12 @@ fputc(c,F);
 i++;
 c=l[i];
 }
-//fprintf(F,"%d",count);
-
-
-
+  
 }
 else if(type==8){
 c=m[0];
 i=0;
-
-/*while(c!='\0'){
-fputc(c,F);
-i++;
-c=m[i];
-}
-*/
-
-
 fprintf(F,"%d",count);
-
-//fputc('<',F);
-//fprintf(F,"%d",count);
-
 }
 
 else if(type==9){
@@ -995,8 +924,6 @@ i++;
 c=n[i];
 }
 //
-//fputc('\n',F);
-//fputc('\t',F);
 }
 //
 return;
